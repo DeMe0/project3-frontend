27efb016e457c9eb1c00b85f20742c893aeef19b
redpumpkin21 <christopher.johnson2108@gmail.com>
2021-08-23 12:37:53 -0400

    buttons styalized

--- src/Componets/Buttons.js
+++ src/Componets/Buttons.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { Route, Link, Switch } from "react-router-dom";
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 const Button = () => {
     return(
-        <>
+        <div className = "menubutton">
         <Link to ='/drinks'>
             <button className ='drinks'>
-            
+                <AiOutlineArrowRight />
             </button>
         </Link>
         <Link to ='/icecreams'>
             <button className ='drinks'>
-
+            <AiOutlineArrowRight />
             </button>
         </Link>
         <Link to ='/italianices'>
             <button className ='drinks'>
-
+            <AiOutlineArrowRight className = 'icearrow'/>
             </button>
         </Link>
         <Link to ='/popsicles'>
             <button className ='drinks'>
-
+            <AiOutlineArrowRight />
             </button>
         </Link>
-        </>
+        </div>
     )
 }
 
