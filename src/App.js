2bf9e79f68c02d904ce9a1a9d75209b430734d89
redpumpkin21 <christopher.johnson2108@gmail.com>
2021-08-23 16:10:41 -0400

    menu works

--- src/App.js
+++ src/App.js
@@ -1,62 +1,80 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Link, Switch } from "react-router-dom";
-import Main from './Main'
-import Navigation from './Nav'
+import Main from "./Main";
+import Navigation from "./Nav";
+import Menu from "./Pages/Menu";
+import Drink from "./Pages/Drink";
+import IceCream from "./Pages/IceCream";
+import ItalianIce from "./Pages/ItalianIce";
+import Popsicle from "./Pages/Popsicle";
+import Button from "./Componets/Buttons";
 
 function App() {
-  const url="https://project3-icecream.herokuapp.com"
-  const[parlours,setParlours]= useState([])
-  const getParlours = ()=>{ 
+  const url = "https://project3-icecream.herokuapp.com";
+  const [parlours, setParlours] = useState([]);
+  const getParlours = () => {
     fetch(url + "/parlours")
-    .then((response) => response.json())
-    .then((data) => setParlours(data))
-  }
+      .then((response) => response.json())
+      .then((data) => setParlours(data));
+  };
   useEffect(() => getParlours(), []);
 
-  const[icecreams,setIcecreams]= useState([])
-  const getIcecreams= ()=>{ 
+  const [icecreams, setIcecreams] = useState([]);
+  const getIcecreams = () => {
     fetch(url + "/icecream")
-    .then((response) => response.json())
-    .then((data) => setIcecreams(data))
-  }
-  
+      .then((response) => response.json())
+      .then((data) => setIcecreams(data));
+  };
+
   useEffect(() => getIcecreams(), []);
 
-  const[popsicles,setPopsicles]= useState([])
-  const getPopsicles= ()=>{ 
+  const [popsicles, setPopsicles] = useState([]);
+  const getPopsicles = () => {
     fetch(url + "/popsicles")
-    .then((response) => response.json())
-    .then((data) => setPopsicles(data))
-  }
-  
+      .then((response) => response.json())
+      .then((data) => setPopsicles(data));
+  };
+
   useEffect(() => getPopsicles(), []);
 
-  const[ice,setIce]= useState([])
-  const getIce= ()=>{ 
+  const [ice, setIce] = useState([]);
+  const getIce = () => {
     fetch(url + "/ice")
-    .then((response) => response.json())
-    .then((data) => setIce(data))
-  }
-  
-  useEffect(() => getIce(), []);
-   
+      .then((response) => response.json())
+      .then((data) => setIce(data));
+  };
 
+  useEffect(() => getIce(), []);
 
   return (
     <div className="App">
-      <img className="logo" src="https://res.cloudinary.com/dt5zbnwvr/image/upload/v1629589890/homemade_utscyu.png"></img>
-     <main>
-     <Route exact path="/"
-            render={(rp) => <Main {...rp} parlours={parlours} 
-            />}
-    />
-     <Route path="/menu"></Route>
-     <Route path="/offers"></Route>
-     <Route path="/about"></Route>
-     <Route path="/cart"></Route>
-     </main>
-     <Navigation/>
+      <img
+        className="logo"
+        src="https://res.cloudinary.com/dt5zbnwvr/image/upload/v1629589890/homemade_utscyu.png"
+      ></img>
+      <main>
+        <Route
+          exact
+          path="/"
+          render={(rp) => <Main {...rp} parlours={parlours} />}
+        />
+        <Route
+          path="/menu"
+          render={(rp) => <Menu {...rp} parlours={parlours} />}
+        >
+          <Button />
+        </Route>
+        <Route exact path="/drinks" render={() => <Drink />}></Route>
+
+        <Route exact path="/icecreams" render={() => <IceCream />}></Route>
+        <Route exact path="/italianices" render={() => <ItalianIce />}></Route>
+        <Route exact path="/popsicles" render={() => <Popsicle />}></Route>
+        <Route path="/offers"></Route>
+        <Route path="/about"></Route>
+        <Route path="/cart"></Route>
+      </main>
+      <Navigation />
     </div>
   );
 }
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -8,34 +8,7 @@ import Popsicle from './Popsicle';
 import Button from '../Componets/Buttons'
 const Menu = () => {
     return(
-        <>
-               <main>
-            <Switch>
-                <Route
-                    exact path ='/menu'
-                    render={() => (
-                        <Button />
-                    )}>
-                </Route>
-                <Route exact path='/drinks'
-                render={() => (
-                    <Drink />
-                  )}></Route>
-                   <Route exact path='/icecreams'
-                render={() => (
-                    <IceCream />
-                  )}></Route>
-                   <Route exact path='/italianices'
-                render={() => (
-                    <ItalianIce />
-                  )}></Route>
-                   <Route exact path='/popsicles'
-                render={() => (
-                    <Popsicle />
-                  )}></Route>
-            </Switch>
-        </main>
-        </>
-    )
+        <h1>hey</h1>
+       )
 }
 export default Menu
