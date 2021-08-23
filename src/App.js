8db1f5f53e6f1a9234fe1c743562b449745f0ee0
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-08-22 21:44:45 -0500

    homepage initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Link, Switch } from "react-router-dom";
+import Main from './Main'
 
 function App() {
   return (
     <div className="App">
-      <h1>Front-End Test Text</h1>
+      <img className="logo" src="https://res.cloudinary.com/dt5zbnwvr/image/upload/v1629589890/homemade_utscyu.png"></img>
+    <main>
+     <Route exact path="/">
+     <Main/>
+     </Route>
+     
+
+
+    </main>
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+  } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const items = [
+    {
+      src: 'https://www.theflavorbender.com/wp-content/uploads/2020/07/Ube-Ice-Cream-SM-8065.jpg',
+      altText: 'Ube Ice Cream',
+    //   caption: 'Trader Joe‘s',
+      header: 'Ube Ice Cream',
+    },
+    {
+      src: 'https://ucarecdn.com/52f4c5f4-bd63-4165-8543-40fc5ae249ae/',
+      altText: 'Everything Bagel',
+    //   caption: 'Jeni‘s',
+      header: 'Everything Bagel',
+    },
+    {
+      src: 'https://d2aam04nmhpdf8.cloudfront.net/images/images/000/063/238/xlarge/wxfmhi82lpz0ogqck4jl.png?1625262112',
+      altText: 'Cake Batter Vodka Martini',
+    //   caption: 'Tipsy Scoop',
+      header: 'Cake Batter Vodka Martini',
+    },
+    {
+        src: 'https://wellsmanagedcontent.azureedge.net/content/blue-bunny/product/thumbnail1/92cee2ab-ac6a-4be6-8da9-4da8ebe93d5d/bunny-tracks.v1.png',
+        altText: 'Bunny Tracks',
+        // caption: 'Blue Bunny Ice Cream',
+        header: 'Bunny Tracks',
+      }
+  ];
+
+const Selection = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+  
+    const next = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const previous = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const goToIndex = (newIndex) => {
+      if (animating) return;
+      setActiveIndex(newIndex);
+    }
+  
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={() => setAnimating(true)}
+          onExited={() => setAnimating(false)}
+          key={item.src}
+        >
+          <img className="carousel-image" src={item.src} alt={item.altText} />
+          <CarouselCaption className="caption"captionHeader={item.header} />
+        </CarouselItem>
+      );
+    });
+  
+    return (
+      <Carousel className="Carousel"
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+    );
+  }
+  
+
+export default Selection
