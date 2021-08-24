43847036f0ef6851e876cb20941bdf7a04d967e3
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-08-24 13:37:28 -0500

    all products displayed

--- src/Pages/Drink.js
+++ src/Pages/Drink.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartArrowDown} from '@fortawesome/free-solid-svg-icons';
+
 const Drink = (props) => {
     const {drinks} = props      
         const loaded = () => (
             <div  className = 'drinks'>
-            {drinks.map((drink) => (               
-               <article>
-                <img src={drink.img} />
-                 <h1>{drink.brand}</h1>
-                 <p>{drink.flavor}</p>
-                 <p>{drink.price}</p>
-                
-              </article>
-           ))}
-           <Link to ='/menu'>
+            <Link to ='/menu'>
                     <button className ='menu'>
                     <AiOutlineArrowLeft />
                     </button>
                      </Link>
+            {drinks.map((drink) => (               
+               <article>
+                <img src={drink.img} />
+                <h1>{drink.brand}</h1>
+                <p>{drink.flavor}</p>
+                <p> $ {drink.price} <FontAwesomeIcon className="cart" icon={faCartArrowDown}/> </p>
+              </article>
+           ))}
               </div>
                )
            
