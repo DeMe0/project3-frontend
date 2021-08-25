5bb2291419414e70bfdd16c1f31a11ff1c2fddef
Anthony DeMeo <anthonydemeo@anthonys-mbp.home>
2021-08-25 17:26:03 -0400

    updated cart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import Items from "../Componets/Items";
 
 const Cart = (props) => {
-  return (
+  const loaded = () => (
     <div className="mycart">
       <h2 id="mycartTitle">My Cart</h2>
       <div className="cartBorderWrap">
         <div className="cartContainer">
+          <div className="cartColumnNames">
+            <h4>Product</h4>
+            <h4>Price</h4>
+          </div>
+          <div className="separator"></div>
           {/* map over items in our cart and print their */}
           {props.cart.map((item) => {
             return (
@@ -16,10 +21,32 @@ const Cart = (props) => {
               </div>
             );
           })}
+          <div className="separator"></div>
+          <div className="cartLine">
+            <h3>Subtotal</h3>
+            <h3>
+              $
+              {props.cart
+                .map((price) => price.price)
+                .reduce((acc, price) => price + acc)}
+            </h3>
+          </div>
         </div>
       </div>
     </div>
   );
+  const loading = (
+    <div className="mycart">
+      <h2 id="mycartTitle">My Cart</h2>
+      <div className="cartBorderWrap">
+        <div className="cartContainer">
+          <h3>Cart Is Empty</h3>
+        </div>
+      </div>
+      <div className="separator"></div>
+    </div>
+  );
+  return props.cart.length > 0 ? loaded() : loading;
 };
 
 export default Cart;
