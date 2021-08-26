dc36ab3dd38cafb09eb6f4653b7cd79b5dcdec8a
redpumpkin21 <christopher.johnson2108@gmail.com>
2021-08-26 10:24:31 -0400

    checkout modal

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -24,13 +24,13 @@ const Checkout= (props) => {
 
   return (
     <div>
-      <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+      <Button id= 'checkoutbutton' color="danger" onClick={toggle}>Checkout!{buttonLabel}</Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader  id='title'>YOUR ORDER</ModalHeader>
         <ModalBody>
-          {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. */}
+          
           <div>
-            <Countdown date={Date.now() + 1800000}>
+            <Countdown style ={{"fontSize": "32px"}} date={Date.now() + 1800000}>
       <Completionist />
     </Countdown>
         <h1>Thank you for your purchase</h1>
@@ -39,6 +39,7 @@ const Checkout= (props) => {
         
       
         </div>
+
         </ModalBody>
         <ModalFooter>
         <Link to="/">
