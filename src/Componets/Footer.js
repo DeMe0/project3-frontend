2fc43b8a614d889fded19e3eeba33cb1aafaacba
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-08-26 11:49:34 -0500

    footer

--- src/Componets/Footer.js
+++ src/Componets/Footer.js
@@ -1,6 +1,11 @@
 const Footer = () => {
     return(
-        <h1 className='Footer'>Copyright, don't steal be kind</h1>
+        <div className="footer">
+        <hr></hr>
+        <h2>Ice Cream Brings Us Together</h2>
+        <h4>Copy Right @ Anthony Demeo, Christopher Johnson & Stephanie Liu</h4>
+        <img src="https://i.pinimg.com/originals/25/7e/03/257e031b4ffd8845adcb67d53e0fe61d.gif"></img>
+        </div> 
     )
 }
 export default Footer
